1a8619922127e53455597bd7e2db3c100df2bd93
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-15 21:27:59 -0400

    async to query riot api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var PORT_NUMBER = process.env.PORT || 5000;
 
 // RIOT API key(?) and URL setup
 //var secret = require('secret.js');
-var API_KEY = '';
+var API_KEY = process.env.API_KEY;
 var API_URL = 'https://na.api.pvp.net';
 
 // Dependencies
@@ -25,26 +25,30 @@ app.use('/bower_components',
 app.use('/static',
         express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({ extended: true }));
-// CONFIGURATION ===============================================================
 
 
 // ROUTING =====================================================================
 app.get('/', function(request, response) {
-  var championIDs;
-  https.get(API_URL + '/api/lol/na/v1.2/champion?api_key=' + API_KEY, function(response){
-  var data = '';
-  response.on('data', function(chunk){
-    data += chunk;
+  var data = null;
+
+  async.parallel([
+    function(callback) {
+      https.get(API_URL + '/api/lol/na/v1.2/champion?api_key=' + API_KEY,
+                function(res) {
+        res.on('data', function(chunk) {
+          data += chunk;
+        });
+        res.on('end',function() {
+          callback();
+        });
+      });
+    }
+  ], function(error) {
+    response.render('index.html', {
+      championIDs: data
+    });
   });
-  response.on('end',function(){
-    championIDs = JSON.parse(data);
-    console.log(championIDs);
-  })
 });
-  //console.log(championIDs);
-  response.render('index.html', {championIDs:championIDs});
-});
-// ROUTING =====================================================================
 
 
 // SERVER SETUP ================================================================
@@ -52,4 +56,3 @@ var server = require('http').Server(app);
 server.listen(PORT_NUMBER, function() {
   console.log('Listening to port ' + PORT_NUMBER);
 });
-// SERVER SETUP ================================================================
