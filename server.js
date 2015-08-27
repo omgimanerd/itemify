91e7d8b74d4b5e7f1058e039657f9f4350ede561
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-27 09:42:33 -0400

    moved image assets from dataset to image directory

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.set('view engine', 'html');
 
 app.use('/bower_components',
         express.static(__dirname + '/bower_components'));
+app.use('/images',
+        express.static(__dirname + '/images'));
 app.use('/static',
         express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({ extended: true }));
