b249cb05413674557fc0b68af55c69f3b621175b
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-30 21:00:59 -0400

    reformatted front-end, made download links

--- server.js
+++ server.js
@@ -1,54 +1,61 @@
 var PORT_NUMBER = process.env.PORT || 5000;
 
-// Dependencies
+// DEPENDENCIES
 var async = require('async');
 var bodyParser = require('body-parser');
 var express = require('express');
 var fs = require('fs');
+var morgan = require('morgan');
 var swig = require('swig');
 var https = require('https');
 
 var app = express();
 
-// CONFIGURATION ===============================================================
+// CONFIGURATION
 app.engine('html', swig.renderFile);
 
 app.set('port', PORT_NUMBER);
 app.set('view engine', 'html');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
 app.use('/bower_components',
         express.static(__dirname + '/bower_components'));
+app.use('/builds',
+        express.static(__dirname + '/dataset/builds'));
+app.use('/build_images',
+        express.static(__dirname + '/dataset/build_images'));
 app.use('/static',
         express.static(__dirname + '/static'));
-app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// ROUTING =====================================================================
+// ROUTING
 app.get('/', function(request, response) {
   response.render('index.html');
 });
 
-app.get('/champ', function(request, response) {
+app.get('/champion', function(request, response) {
   champJSON = null;
 
-  async.parallel([
-    function(callback) {
-      fs.readFile('dataset/stats-by-champion/' + request.path + '.json',
-                  function(err, data) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        champJSON = data;
-        response.send(champJSON);
-      });
-    }
-  ], function(error) {
+  response.render('champion.html', {
+    champion: request.query.champion
   });
+//  async.parallel([
+//    function(callback) {
+//      fs.readFile('dataset/builds/' + request.query.champion + '.json',
+//                  function(err, data) {
+//        if (err) {
+//          console.log(err);
+//          return;
+//        }
+//        champJSON = data;
+//        response.send(champJSON);
+//      });
+//    }
+//  ], function(error) {
+//  });
 });
 
-
-// SERVER SETUP ================================================================
+// SERVER SETUP
 var server = require('http').Server(app);
 server.listen(PORT_NUMBER, function() {
   console.log('Listening to port ' + PORT_NUMBER);
