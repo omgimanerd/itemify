ffbc66ab1a4aad70a2ba30d42b2846f2b6355358
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-30 22:09:49 -0400

    added champion build images

--- dataset/scripts/crop_build_images.py
+++ dataset/scripts/crop_build_images.py
@@ -6,6 +6,25 @@
 # direction were freshly loaded from the League of Legends Screenshot directory,
 # with one image for each champion build prenamed in numerical order corresponding
 # to the champions' alphabetical order.
+# This script must be run inside the build_images folder since it uses the os
+# module which works using the current terminal context.
 # Author: Alvin Lin (alvin.lin@stuypulse.com)
 
+import json
 from PIL import Image
+import os
+
+def main():
+  with open('../champions.json') as champions_input:
+    champions = json.loads(champions_input.read())
+  champion_names = sorted(champions, key=lambda x: champions[x]['name'])
+
+  i = 1
+  for name in champion_names:
+    print 'mv Screen{:02d} {}_build.png'.format(i, name.lower())
+    os.system('mv Screen{:02d}.png {}_build.png'.format(i, name.lower()))
+    i += 1
+
+
+if __name__ == '__main__':
+  main()
