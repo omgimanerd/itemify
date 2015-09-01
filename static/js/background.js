f337497821ab9dbe3d75d70b1ec4b5e7c60b410f
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-31 21:18:14 -0400

    updated gitignore

---
+++ static/js/background.js
@@ -0,0 +1,8 @@
+/**
+ * This is the script that animates the background to fade between images.
+ * @author Alvin Lin (alvin.lin@stuypulse.com)
+ */
+
+$(document).ready(function() {
+  setInterval(
+});
