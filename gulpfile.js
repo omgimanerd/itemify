192343273f6e68b04491a8ca50c3e8f2c0e1a966
Alvin Lin <alvin.lin@stuypulse.com>
2015-09-25 20:08:50 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+var notify = require('gulp-notify');
 
 gulp.task('less', function() {
   console.log('Recompiling CSS');
   return gulp.src('./static/less/styles.less')
-    .pipe(less())
-    .pipe(gulp.dest('./static/css'));
+    .pipe(less({ compress: true }))
+    .pipe(autoprefixer({ browsers: ['last 10 versions']}))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./static/css'))
+    .pipe(notify('LESS compiled and minified'));
 });
 
 gulp.task('watch-less', function() {
